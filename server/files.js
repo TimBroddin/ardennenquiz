8992582c7955954f2491739432a09aab2a4b26ec
Tim Broddin <tim.broddin@vrt.be>
2018-02-22 14:52:45 +0100

    initial

--- server/files.js
+++ server/files.js
@@ -1,16 +1,17 @@
 Slingshot.createDirective("files", Slingshot.S3Storage, {
-  bucket: "oudjaarsquiz",
+  bucket: "ardennenquiz",
   acl: "public-read",
   region: "eu-west-1",
 
-
-  authorize: function () {
+  authorize: function() {
     return true;
   },
 
-  key: function (file) {
-    return `${ new Date().getTime()}_${Math.round(Math.random()*1000)}_${file.name}`;
+  key: function(file) {
+    return `${new Date().getTime()}_${Math.round(Math.random() * 1000)}_${
+      file.name
+    }`;
   },
-    allowedFileTypes: null,
-  	maxSize: 100 * 1024 * 1024 // 10 MB (use null for unlimited).
+  allowedFileTypes: null,
+  maxSize: 100 * 1024 * 1024 // 10 MB (use null for unlimited).
 });
